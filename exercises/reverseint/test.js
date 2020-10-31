d3765df1553ef223bd8f2c197bccaefb40c009de
Nicole Latifi <Nicole.Latifi@gmail.com>
2020-10-30 21:02:02 -0600

    Solve exercise

--- exercises/reverseint/test.js
+++ exercises/reverseint/test.js
@@ -1,21 +1,21 @@
 const reverseInt = require('./index');
 
-describe.skip('Reverse Int', () => {
+describe('Reverse Int', () => {
  it('ReverseInt function exists', () => {
     expect(reverseInt).toBeDefined();
   });
-  
+
  it('ReverseInt handles 0 as an input', () => {
     expect(reverseInt(0)).toEqual(0);
   });
-  
+
  it('ReverseInt flips a positive number', () => {
     expect(reverseInt(5)).toEqual(5);
     expect(reverseInt(15)).toEqual(51);
     expect(reverseInt(90)).toEqual(9);
     expect(reverseInt(2359)).toEqual(9532);
   });
-  
+
  it('ReverseInt flips a negative number', () => {
     expect(reverseInt(-5)).toEqual(-5);
     expect(reverseInt(-15)).toEqual(-51);
